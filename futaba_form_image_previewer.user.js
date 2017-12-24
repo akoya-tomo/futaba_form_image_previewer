b62c63789e10d586eff9d47003e3746bb91bd619
akoya-tomo <akoya_tomo-00755@yahoo.co.jp>
2017-12-24 21:22:27 +0900

    v0.2.0

--- futaba_form_image_previewer.user.js
+++ futaba_form_image_previewer.user.js
@@ -8,7 +8,7 @@
 // @include        http://*.2chan.net/*/futaba.htm
 // @include        https://*.2chan.net/*/futaba.htm
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
-// @version        0.1.1
+// @version        0.2.0
 // @grant          none
 // @license        MIT
 // ==/UserScript==
